86b120344a7957ef4ecb4c28e2fe908b961f00b4
moritzebeling <git@moritzebeling.com>
2021-01-24 12:13:42 +0100

    implement buy sdk

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
   import Product from "./components/Product.svelte";
   import CollectionButton from "./components/CollectionButton.svelte";
   import Storefront from "./components/Storefront.svelte";
+  import Buy from "./components/Buy.svelte";
 
 </script>
 
 <!-- <Product /> -->
 <!-- <CollectionButton /> -->
-<Storefront />
+<!-- <Storefront /> -->
+<Buy />
 
 <style lang="scss">
 
